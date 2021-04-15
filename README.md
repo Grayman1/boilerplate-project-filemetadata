3587cf5dfecd48485d62b145ad2ddcd626025e3d
Grayman1 <>
2021-04-15 15:34:11 +0000

    All tests PASS! Add multer, coding Modify index.html, stylecss

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+User Stories:
+
+1. You should provide your own project, not the example URL.
+
+2. You can submit a form that includes a file upload.
+
+3. The form file input field has the name attribute set to upfile.
+
+4. When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
--- server.js
+++ server.js
@@ -18,3 +18,22 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+
+
+// 1st SOLUTION ATTEMPT
+let multer = require('multer')
+
+//call multer -  no options
+// specify single file
+// placeholder upfile
+
+
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  let resObject = {}
+  resObject['name'] = req.file.originalname
+  resObject['type'] = req.file.mimetype
+  resObject['size'] = req.file.size
+
+  res.json(resObject)
+})
